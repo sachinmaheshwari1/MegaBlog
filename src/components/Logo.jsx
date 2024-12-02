aef7f1236f9f157bfbd41ee64f79c842cd2acf9f
sachinmeshwari1 <sahesheari@gmail.com>
2024-12-02 15:00:16 +0530

    megablog project added

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <img src="https://www.svgrepo.com/show/508912/drupal.svg" className='w-[10vw] h-[4vw] mt-3' alt="Logo" style={{filter:"brightness(0) saturate(100%) invert(100%) sepia(96%) saturate(15%) hue-rotate(212deg) brightness(104%) contrast(104%)"}} />
+  )
+}
+
+export default Logo
